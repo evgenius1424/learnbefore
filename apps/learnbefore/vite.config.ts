ae4930e3d420282e929067d7c09dc8c97e9c53f8
evgenius1424 <evgenius1424@yandex.ru>
2024-04-17 22:58:34 +0200

    Update build target in vite.config and clean up workspace.xml
    The ESNext target was set in the Vite configuration to better future-proof the application. Additionally, unnecessary lines were removed from workspace.xml and some other minor settings were adjusted.

--- apps/learnbefore/vite.config.ts
+++ apps/learnbefore/vite.config.ts
@@ -4,6 +4,9 @@ import path from "node:path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [react()],
   resolve: {
     alias: {
