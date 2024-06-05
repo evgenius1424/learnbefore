dd6a9e91d392d44050c483a84c1a0aeeb2edc417
evgenius1424 <evgenius1424@yandex.ru>
2024-06-05 15:34:47 +0200

    Remove body-parser dependency
    Body-parser has been removed from the package.json file and its usage from start-express.ts. In start-express.ts, body-parser.json() has been replaced with express.json() and express.urlencoded() with limit specifications.

--- apps/learnbefore-bff/src/start-express.ts
+++ apps/learnbefore-bff/src/start-express.ts
@@ -1,14 +1,13 @@
 import express, { Express } from "express"
-import bodyParser from "body-parser"
-
 import cors from "cors"
 
 export function startExpress(port: number, origin: string = "*"): Express {
   const server: Express = express()
 
-  server.use(express.json())
+  server.use(express.json({ limit: "1mb" }))
+  server.use(express.urlencoded({ limit: "1mb", extended: true }))
   server.use(cors({ origin }))
-  server.use(bodyParser.json({ limit: "200kb" }))
   server.listen(port, () => console.log(`Server ready on port ${port}.`))
+
   return server
 }
