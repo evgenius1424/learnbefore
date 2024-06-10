bdfe5d47a5148a2bf6ac50ce8591e56b653b5b72
evgenius1424 <evgenius1424@yandex.ru>
2024-06-10 22:35:46 +0200

    Add error handling middleware to Express server
    This commit includes the necessary imports to handle errors at the server level in Express. If there's an unexpected error, Express now logs the error stack to the console and responds with a 500 status code and an "Unexpected server error" message.

--- apps/learnbefore-bff/src/start-express.ts
+++ apps/learnbefore-bff/src/start-express.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express"
+import express, { Express, NextFunction, Request, Response } from "express"
 import cors from "cors"
 
 export function startExpress(port: number, origin: string = "*"): Express {
@@ -7,6 +7,12 @@ export function startExpress(port: number, origin: string = "*"): Express {
   server.use(express.json({ limit: "1mb" }))
   server.use(express.urlencoded({ limit: "1mb", extended: true }))
   server.use(cors({ origin }))
+
+  server.use((err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack)
+    res.status(500).send("Unexpected server error")
+  })
+
   server.listen(port, () => console.log(`Server ready on port ${port}.`))
 
   return server
