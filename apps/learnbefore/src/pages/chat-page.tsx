c03cc823fd851b466cb0657b09a679933ae3496c
evgenius1424 <evgenius1424@yandex.ru>
2024-06-14 00:03:37 +0200

    Add paperclip icon to chat-page.

--- apps/learnbefore/src/pages/chat-page.tsx
+++ apps/learnbefore/src/pages/chat-page.tsx
@@ -161,6 +161,12 @@ export const ChatPage: React.FC = () => {
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
             />
+            <label>
+              <Button variant="ghost" size="icon">
+                <PaperclipIcon className="w-4 h-4" />
+              </Button>
+              <input type="file" className="hidden" aria-hidden="true" />
+            </label>
             <Button disabled={sendInProgress} type="submit">
               Send
             </Button>
@@ -186,3 +192,24 @@ const WordCard: React.FC<{ word: Word }> = ({ word }) => (
     </CardContent>
   </Card>
 )
+
+type IconProps = React.HTMLAttributes<SVGElement>
+
+function PaperclipIcon(props: IconProps) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m21.44 11.05-9.19 9.19a6 6 0 0 1-8.49-8.49l8.57-8.57A4 4 0 1 1 18 8.84l-8.59 8.57a2 2 0 0 1-2.83-2.83l8.49-8.48" />
+    </svg>
+  )
+}
