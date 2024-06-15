369802045aa9f7bac498d2038586e761561ec3ae
evgenius1424 <evgenius1424@yandex.ru>
2024-06-15 16:03:33 +0200

    Update prompt

--- apps/learnbefore-bff/src/get-words.ts
+++ apps/learnbefore-bff/src/get-words.ts
@@ -46,7 +46,7 @@ export async function* getWords(
 }
 
 const systemPrompt =
-  "Use only RFC8259 compliant compact JSON. The aim is to extract a large list of words that a regular language learner is unlikely to know, but at the same time the word should be widely known. So avoid names of any kind, geo-locations or other proper nouns. Duplicates are not allowed, words should be provided in dictionary forms. " +
+  "Use only RFC8259 compliant compact JSON. The goal is to extract from user text a large list of words that a regular language learner is unlikely to know, but at the same time the word should be widely known. So avoid names of any kind, geo-locations or other proper nouns. Duplicates are not allowed, words should be provided in dictionary forms. " +
   "Words that do not exist in the text are not allowed. Returns an empty response if the text contains no words. " +
   "Good words: Coyly, teeming, attic, scorching - because these words are rarely used. " +
   "Bad words: Swiss Institute, Natalia - because they're not dictionary words, they're names."
@@ -54,7 +54,7 @@ const systemPrompt =
 function getUserPrompt(text: string, translationLanguage = "Russian") {
   return `
     You must extract 40 words from the text below which language learner likely do not know or need to know in order to understand the text. 
-    Please ensure the extracted words are diverse and relevant to the context of the text.
+    Please ensure the extracted words are contained in the text.
     
     Translation language is ${translationLanguage}.
     
