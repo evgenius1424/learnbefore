9606d3d752b5496f7a39bb6f246064db373fbd57
evgenius1424 <evgenius1424@yandex.ru>
2024-06-15 20:39:50 +0200

    fix main.tsx

--- apps/learnbefore/src/main.tsx
+++ apps/learnbefore/src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom/client"
 import App from "./app.tsx"
 import "./../src/globals.css"
-import React from "react"
 import { ClerkProvider } from "@clerk/clerk-react"
+import React from "react"
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -11,9 +11,10 @@ if (!PUBLISHABLE_KEY) {
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <App />
-  </ClerkProvider>,
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+    ,
+  </React.StrictMode>,
 )
