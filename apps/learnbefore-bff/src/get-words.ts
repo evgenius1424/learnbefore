8f1e0eb047b439f57bd209ef7336eb71616755a7
evgenius1424 <evgenius1424@yandex.ru>
2024-10-27 12:36:36 +0100

    Add ordering info to prompt.

--- apps/learnbefore-bff/src/get-words.ts
+++ apps/learnbefore-bff/src/get-words.ts
@@ -47,11 +47,11 @@ export async function* getWords(
 
 const systemPrompt =
   "Use only RFC8259 compliant compact JSON and help to extract big list of words from the text that the language learner is unlikely to know or that are crucial to the understanding of the text. Words should be converted to dictionary form. Duplicates, names of characters, persons or toponyms are not allowed." +
-  "Words that do not exist in the text are not allowed. Returns an empty response if the text contains no words."
+  "Words that do not exist in the text are not allowed. Returns an empty response if the text contains no words. MUST keep the order of words as they appear in the text."
 
 function getUserPrompt(text: string, translationLanguage = "Russian") {
   return `
-    You must extract 40 words from the text below which language learner likely do not know or need to know in order to understand the text. 
+    You must extract 50 words from the text below which language learner likely do not know or need to know in order to understand the text. 
     Please ensure the extracted words are diverse and relevant to the context of the text.
     
     Translation language is ${translationLanguage}.
