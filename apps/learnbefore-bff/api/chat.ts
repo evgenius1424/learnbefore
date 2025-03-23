1c68e72b4fc3ceb7613d1277ae8899c32355bda2
evgenius1424 <evgenius1424@yandex.ru>
2025-03-23 18:49:11 +0100

    feat: extract store to separate interface

--- apps/learnbefore-bff/api/chat.ts
+++ apps/learnbefore-bff/api/chat.ts
@@ -2,13 +2,14 @@ import { Request, Response } from "express"
 import OpenAI from "openai"
 import { getWords } from "../src/get-words"
 import { Message, User } from "../types"
-import { Store } from "../src/store"
+import { MongoDbStore } from "../src/mongo-db-store"
 import { splitText } from "../src/split-text"
 import { equalsIgnoringCase } from "../src/equals-ignoring-case"
 import { retryableGenerator } from "../src/retryable-generator"
 import { startExpress } from "../src/start-express"
 import { requireAuth } from "@clerk/express"
 import expressAsyncHandler from "express-async-handler"
+import { Store } from "../src/store"
 
 declare global {
   // eslint-disable-next-line no-unused-vars
@@ -22,7 +23,8 @@ declare global {
 
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 const app = startExpress(parseInt(process.env.EXPRESS_PORT || "3000"))
-const store = new Store(process.env.MONGO_CONNECTION_STRING!!)
+
+const store: Store = new MongoDbStore(process.env.MONGO_CONNECTION_STRING!!)
 
 const cachedUsers: Record<string, User> = {}
 
