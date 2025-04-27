1a6fd6f1fd221b138d3edc34bb51332bb22459a8
evgenius1424 <evgenius1424@yandex.ru>
2025-04-27 19:19:38 +0200

    feat: add initial setup for Supabase and Clerk integration

--- apps/learnbefore-next/app/page.tsx
+++ apps/learnbefore-next/app/page.tsx
@@ -1,20 +1,47 @@
-"use client";
-import React, { useState } from "react";
-import { Button } from "@repo/ui/components/button";
+"use client"
+import React, { useState, useEffect } from "react"
+import { Button } from "@repo/ui/components/button"
+import { createClient } from "@supabase/supabase-js"
+import { useSession } from "@clerk/nextjs"
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export default function Page() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+  const { session } = useSession()
+
+  const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+    accessToken: async () => {
+      console.log("Token requested")
+      session?.getToken().then((token) => {
+        console.log("Token received", token)
+      })
+      return session ? session.getToken() : null
+    }
+  })
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await supabase.from("test").select("id")
+      console.log(data)
+    }
+
+    if (session) {
+      fetchData()
+    }
+  }, [session, supabase]) // Re-fetch when the session changes
 
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-xl font-bold mb-2">This is a Next.js application</h1>
       <p className="mb-4">
-        This shadcn/ui button is shared between Next.js, Vite and any other
+        This shadcn/ui button is shared between Next.js, Vite, and any other
         application.
       </p>
       <Button onClick={() => setCount((count) => count + 1)}>
         Count is {count}
       </Button>
     </main>
-  );
+  )
 }
